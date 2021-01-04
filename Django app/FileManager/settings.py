f83cc8f55e24e92a8bf858e80cbfa618d670c864
Monis Bana <monisbana97@gmail.com>
2021-01-04 21:01:51 +0530

    Removed Session auth

--- Django app/FileManager/settings.py
+++ Django app/FileManager/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
